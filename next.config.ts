b94d25af3685ad2ca41fb70e03f47965e3bb6087
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2025-01-20 23:24:03 +0300

    ..

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_URL || '',
   /* config options here */
 };
 
