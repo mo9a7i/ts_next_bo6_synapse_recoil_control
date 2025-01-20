af6f75fd7b3956a73169ac14a2376f86432c8ab5
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2025-01-20 23:34:34 +0300

    Update page.tsx

--- src/app/[locale]/weapons/[id]/page.tsx
+++ src/app/[locale]/weapons/[id]/page.tsx
@@ -5,8 +5,9 @@ import { join } from "path";
 import { weapons } from "@/data/weapons";
 import { notFound } from "next/navigation";
 
+type tParams = Promise<{ id: string }>;
 
-export default async function WeaponConfigPage({ params }: { params: { id: string } }): Promise<React.ReactNode> {
+export default async function WeaponConfigPage( params : tParams) {
     const { id } = await params;
     const weapon = weapons.find(w => w.id === id);
     if (!weapon) notFound();
